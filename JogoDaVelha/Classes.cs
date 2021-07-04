0bc32532b3ac1df9db0bea36952e8eadcf146e2c
viniciusnasc <vini.souza00@gmail.com>
2021-07-04 17:07:18 -0300

    Add project files.

---
+++ JogoDaVelha/Classes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaVelha
+{
+    class Classes
+    {
+        public static void PrintarJogo(string[,] tabuleiro)
+        {
+            Console.WriteLine("   1 2 3");
+            Console.WriteLine("1: " + tabuleiro[0, 0] + "|" + tabuleiro[0, 1] + "|" + tabuleiro[0, 2]);
+            Console.WriteLine("   -----");
+            Console.WriteLine("2: " +tabuleiro[1, 0] + "|" + tabuleiro[1, 1] + "|" + tabuleiro[1, 2]);
+            Console.WriteLine("   -----");
+            Console.WriteLine("3: " +tabuleiro[2, 0] + "|" + tabuleiro[2, 1] + "|" + tabuleiro[2, 2]);
+        }
+
+        public static bool TerminoJogo(string[,] tabuleiro)
+        {
+            bool termino = true;
+            if (tabuleiro [0,0] != " " && tabuleiro[0, 0] == tabuleiro[0, 1] && tabuleiro[0, 0] == tabuleiro[0, 2])
+                termino = true;
+            else if (tabuleiro[0, 0] != " " && tabuleiro[0, 0] == tabuleiro[1, 1] && tabuleiro[0, 0] == tabuleiro[2, 2])
+                termino = true;
+            else if (tabuleiro[0, 0] != " " && tabuleiro[0, 0] == tabuleiro[1, 0] && tabuleiro[0, 0] == tabuleiro[2, 0])
+                termino = true;
+            else if (tabuleiro[0, 1] != " " && tabuleiro[0, 1] == tabuleiro[1, 1] && tabuleiro[0, 1] == tabuleiro[2, 1])
+                termino = true;
+            else if (tabuleiro[0, 2] != " " && tabuleiro[0, 2] == tabuleiro[1, 2] && tabuleiro[0, 2] == tabuleiro[2, 2])
+                termino = true;
+            else if (tabuleiro[0, 2] != " " && tabuleiro[0, 2] == tabuleiro[1, 1] && tabuleiro[0, 2] == tabuleiro[2, 0])
+                termino = true;
+            else if (tabuleiro[1, 0] != " " && tabuleiro[1, 0] == tabuleiro[1, 1] && tabuleiro[1, 0] == tabuleiro[1, 2])
+                termino = true;
+            else if (tabuleiro[2, 0] != " " && tabuleiro[2, 0] == tabuleiro[2, 1] && tabuleiro[2, 0] == tabuleiro[2, 2])
+                termino = true;
+            else
+                termino = false;
+            
+            return termino;
+        }
+
+        public static bool Empate(string[,] tabuleiro)
+        {
+            bool empate = true;
+            for (int i = 0; i < tabuleiro.Length/3; i++)
+            {
+                for (int x = 0; x < tabuleiro.Length/3; x++)
+                {
+                    if (tabuleiro[i, x] == " ")
+                        empate = false;
+                }
+            }
+
+            if (TerminoJogo(tabuleiro) == true)
+                empate = false;
+
+            return empate;
+        }
+    }
+}
---
+++ JogoDaVelha/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace JogoDaVelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] tabuleiro = new string[3, 3] { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " } };
+
+            Console.WriteLine("Seja bem vindo ao jogo da velha!");
+            Classes.PrintarJogo(tabuleiro);
+
+            while (Classes.TerminoJogo(tabuleiro) != true && Classes.Empate(tabuleiro) != true)
+            {
+                // Vez do jogador
+                Console.WriteLine("Digite a posição que ira jogar(Linha,Coluna)");
+                string[] posicao = Console.ReadLine().Split(",");
+                bool validPos1 = int.TryParse(posicao[0], out int posicao1);
+                bool validPos2 = int.TryParse(posicao[1], out int posicao2);
+
+                // Impede valores diferentes de números
+                while (validPos1 == false || validPos2 == false)
+                {
+                    Console.WriteLine("Jogada invalida! Digite uma pposição válida!");
+                    posicao = Console.ReadLine().Split(",");
+                    validPos1 = int.TryParse(posicao[0], out posicao1);
+                    validPos2 = int.TryParse(posicao[1], out posicao2);
+                }
+
+                posicao1 = int.Parse(posicao[0]) - 1;
+                posicao2 = int.Parse(posicao[1]) - 1;
+
+                // Impede valores que ultrapassem a matriz
+                while ((posicao1 >= 3 || posicao1 < 0) || (posicao2 >= 3 || posicao1 < 0))
+                {
+                    Console.WriteLine("Jogada invalida! Digite uma posição válida!");
+                    posicao = Console.ReadLine().Split(",");
+                    validPos1 = int.TryParse(posicao[0], out posicao1);
+                    validPos2 = int.TryParse(posicao[1], out posicao2);
+
+                    while (validPos1 == false || validPos2 == false)
+                    {
+                        Console.WriteLine("Jogada invalida! Digite uma pposição válida!");
+                        posicao = Console.ReadLine().Split(",");
+                        validPos1 = int.TryParse(posicao[0], out posicao1);
+                        validPos2 = int.TryParse(posicao[1], out posicao2);
+                    }
+
+                    posicao1 = int.Parse(posicao[0]) - 1;
+                    posicao2 = int.Parse(posicao[1]) - 1;
+                }
+
+                while (tabuleiro[posicao1, posicao2] != " ")
+                {
+                    Console.WriteLine("Jogada invalida! Digite uma posição válida!");
+                    posicao = Console.ReadLine().Split(",");
+                    validPos1 = int.TryParse(posicao[0], out posicao1);
+                    validPos2 = int.TryParse(posicao[1], out posicao2);
+
+                    while (validPos1 == false || validPos2 == false)
+                    {
+                        Console.WriteLine("Jogada invalida! Digite uma pposição válida!");
+                        posicao = Console.ReadLine().Split(",");
+                        validPos1 = int.TryParse(posicao[0], out posicao1);
+                        validPos2 = int.TryParse(posicao[1], out posicao2);
+                    }
+
+                    posicao1 = int.Parse(posicao[0]) - 1;
+                    posicao2 = int.Parse(posicao[1]) - 1;
+                }
+
+                tabuleiro[posicao1, posicao2] = "O";
+
+                // Vez da maquina
+                if (Classes.TerminoJogo(tabuleiro) != true && Classes.Empate(tabuleiro) != true)
+                {
+                    Random rnd = new();
+
+                    while (tabuleiro[posicao1, posicao2] != " ")
+                    {
+                        posicao1 = rnd.Next(3);
+                        posicao2 = rnd.Next(3);
+                    }
+                    tabuleiro[posicao1, posicao2] = "X";
+                    Classes.PrintarJogo(tabuleiro);
+
+                    if (Classes.TerminoJogo(tabuleiro) == true)
+                        Console.WriteLine("Infelizmente você perdeu! :(");
+                }
+                else
+                {
+                    Classes.PrintarJogo(tabuleiro);
+                    if (Classes.Empate(tabuleiro) == true)
+                        Console.WriteLine("EMPATE!");
+                    else
+                        Console.WriteLine("Parabens, você venceu!");
+                }
+            }
+
+        }
+    }
+}
